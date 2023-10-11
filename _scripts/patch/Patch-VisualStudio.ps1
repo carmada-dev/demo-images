e7ad6efb342aeaa21f227b424cf363735094f662
Markus Heiliger <markus.heiliger@dash-two.de>
2023-10-11 19:50:47 +0200

    ...

--- _scripts/patch/Patch-VisualStudio.ps1
+++ _scripts/patch/Patch-VisualStudio.ps1
@@ -8,6 +8,23 @@ param(
 
 $ProgressPreference = 'SilentlyContinue'	# hide any progress output
 
+function ConvertTo-GUID {
+    
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [string] $Value,
+        [Parameter(Mandatory = $false)]
+        [string] $Format = 'D',
+        [switch] $Raw
+    )
+
+    Using-Object ($md5 = [System.Security.Cryptography.MD5]::Create()) {
+        $buffer = $md5.ComputeHash([System.Text.Encoding]::Default.GetBytes($Value))
+        $guid = [guid]::new($buffer)
+        if ($Raw) { $guid; } else { $guid.ToString($Format); }
+    }
+}
+
 function Using-Object
 {
     param (
