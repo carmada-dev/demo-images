ede02fb20c3746c77fafed306b258a4d0d8ed683
Markus Heiliger <markus.heiliger@dash-two.de>
2024-08-23 13:11:03 +0200

    refactor: Update Install-MSSQL.ps1 to split configuration and enable hibernate support

--- _scripts/Install-MSSQL.ps1
+++ _scripts/Install-MSSQL.ps1
@@ -16,22 +16,48 @@ $ProgressPreference = 'SilentlyContinue'	# hide any progress output
 
 # ==============================================================================
 
-function Patch-ConfigFile() {
+function Split-Configuration() {
 	param(
 		[Parameter(Mandatory=$true, ValueFromPipeline=$true)][string] $Path
 	)
 
-	Write-Host ">>> Patching config: $Path ..."
+	$configPath = $Path
+	$configBlacklist = ('ACTION', 'QUIET', 'QUIETSIMPLE', 'UIMODE', 'UpdateEnabled', 'UpdateSource')
+	$configForceEnabled = ('SUPPRESSPRIVACYSTATEMENTNOTICE', 'SUPPRESSPAIDEDITIONNOTICE', 'ENU')
+	$configForceDisabled = ('HELP', 'INDICATEPROGRESS', 'PRODUCTCOVEREDBYSA')
+	$config = Get-Content -path $configPath | Where-Object { -not([System.String]::IsNullOrEmpty($_)) -and -not("$_".StartsWith(';')) -and -not($configBlacklist -contains ("$_".Split('=')[0])) } | ForEach-Object {
+		$key = "$_".Split('=')[0]
+		if ($configForceEnabled -contains $key) {
+			"$key=`"True`""
+		} elseif ($configForceDisabled -contains $key) {
+			"$key=`"False`""
+		} else {
+			$_
+		}
+	}
+	
+	Write-Host ">>> Preparing PrepareImage configuration ..."
+	$prepareWhitelist = ('ENU', 'PRODUCTCOVEREDBYSA', 'SUPPRESSPRIVACYSTATEMENTNOTICE', 'SUPPRESSPAIDEDITIONNOTICE', 'FEATURES', 'HELP', 'INDICATEPROGRESS', 'INSTALLSHAREDDIR', 'INSTALLSHAREDWOWDIR', 'INSTANCEID', 'INSTANCEDIR')
+	$config | Where-Object { ("$_".StartsWith('[')) -or ($prepareWhitelist -contains ("$_".Split('=')[0])) } | Out-File -FilePath ([System.IO.Path]::ChangeExtension($configPath, '.prepare.ini')) -Force
+	
+	Write-Host ">>> Preparing CompleteImage configuration ..."
+	$completeBlacklist = ('ENU', 'PRODUCTCOVEREDBYSA', 'SUPPRESSPRIVACYSTATEMENTNOTICE', 'SUPPRESSPAIDEDITIONNOTICE', 'FEATURES', 'HELP', 'INDICATEPROGRESS', 'INSTALLSHAREDDIR', 'INSTALLSHAREDWOWDIR', 'INSTANCEID', 'INSTANCEDIR')
+	$config | Where-Object { ("$_".StartsWith('[')) -or -not($completeBlacklist -contains ("$_".Split('=')[0])) } | Out-File -FilePath ([System.IO.Path]::ChangeExtension($configPath, '.complete.ini')) -Force	
 
-	(Get-Content -Path $Path) `
-		-replace "^(ACTION=)", "; $1" `
-		-replace "^(QUIET=)", "; $1" `
-		-replace "^(QUIETSIMPLE=)", "; $1" `
-		-replace "^(UIMODE=)", "; $1" |
-		Out-File -FilePath $Path -Force
+	$Path
 }
 
 if (Test-IsPacker) {
+
+	Invoke-ScriptSection -Title "Prepare Windows Firewall" -ScriptBlock {
+
+		New-NetFirewallRule -DisplayName "SQLServer default instance" -Direction Inbound -LocalPort 1433 -Protocol TCP -Action Allow
+		New-NetFirewallRule -DisplayName "SQLServer Browser service" -Direction Inbound -LocalPort 1434 -Protocol UDP -Action Allow
+	}
+
+	Invoke-ScriptSection -Title "Prepare MSSQL Configuration" -ScriptBlock {
+
+	}
 
 	Invoke-ScriptSection -Title "Prepare MSSQL Server" -ScriptBlock {
 
@@ -40,8 +66,8 @@ if (Test-IsPacker) {
 			Where-Object { Test-Path -Path ([System.IO.Path]::ChangeExtension($_, ".ini")) } |
 			ForEach-Object {
 
-			$configPath = [System.IO.Path]::ChangeExtension($_, ".ini")
-			$configPath | Patch-ConfigFile
+			$configPath = [System.IO.Path]::ChangeExtension($_, ".ini") | Split-Configuration
+			$preparePath = [System.IO.Path]::ChangeExtension($configPath, ".prepare.ini")
 
 			Write-Host ">>> Mounting ISO: $_ ..."
 			Mount-DiskImage -ImagePath $_ -PassThru | ForEach-Object {
@@ -51,10 +77,18 @@ if (Test-IsPacker) {
 				try {
 
 					Write-Host ">>> Prepare MSSQL Server ..."
-					$result = Invoke-CommandLine -Command $setupPath -Arguments "/QUIET /ACTION=PrepareImage /IACCEPTSQLSERVERLICENSETERMS /CONFIGURATIONFILE=`"$configPath`"" 
+					$result = Invoke-CommandLine -Command $setupPath -Arguments "/QUIET /ACTION=PrepareImage /IACCEPTSQLSERVERLICENSETERMS /CONFIGURATIONFILE=`"$preparePath`"" 
 					
 					$result.Output | Write-Host
-					if ($result.ExitCode -ne 0) { Throw "Prepare MSSQL Server failed: $($result.ExitCode)" } 
+					if ($result.ExitCode -ne 0) { 
+					
+						$installPath = "$($config | Where-Object { "$_".StartsWith('INSTANCEDIR=') } | Select-Object -First 1)".Split('=') | Select-Object -Last 1
+						$installPath = "$installPath".Trim().Trim('"')
+						
+						Get-ChildItem -Path $installPath -Filter 'Summary.txt' -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1 | Get-Content | Write-Host
+
+						Throw "Prepare MSSQL Server failed: $($result.ExitCode)" 
+					} 
 
 				} finally {
 
@@ -74,8 +108,8 @@ if (Test-IsPacker) {
 			Where-Object { Test-Path -Path ([System.IO.Path]::ChangeExtension($_, ".ini")) } |
 			ForEach-Object {
 		
-			$configPath = [System.IO.Path]::ChangeExtension($_, ".ini")
-			$configPath | Patch-ConfigFile
+			$configPath = [System.IO.Path]::ChangeExtension($_, ".ini") | Split-Configuration
+			$completePath = [System.IO.Path]::ChangeExtension($configPath, ".complete.ini")
 
 			Write-Host ">>> Mounting ISO: $isoPath ..."
 			Mount-DiskImage -ImagePath $isoPath -PassThru | ForEach-Object {
@@ -85,10 +119,18 @@ if (Test-IsPacker) {
 				try {
 
 					Write-Host ">>> Complete MSSQL Server ..."
-					# $result = Invoke-CommandLine -Command $setupPath -Arguments "/QUIET /ACTION=Install /IACCEPTSQLSERVERLICENSETERMS /CONFIGURATIONFILE=$configPath"
+					$result = Invoke-CommandLine -Command $setupPath -Arguments "/QUIET /ACTION=PrepareImage /IACCEPTSQLSERVERLICENSETERMS /CONFIGURATIONFILE=`"$completePath`""
 
-					# $result.Output | Write-Host
-					# if ($result.ExitCode -ne 0) { Throw "Prepare MSSQL Server failed: $($result.ExitCode)" } 
+					$result.Output | Write-Host
+					if ($result.ExitCode -ne 0) { 
+					
+						$installPath = "$($config | Where-Object { "$_".StartsWith('INSTANCEDIR=') } | Select-Object -First 1)".Split('=') | Select-Object -Last 1
+						$installPath = "$installPath".Trim().Trim('"')
+						
+						Get-ChildItem -Path $installPath -Filter 'Summary.txt' -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1 | Get-Content | Write-Host
+
+						Throw "Prepare MSSQL Server failed: $($result.ExitCode)" 
+					} 
 
 				} finally {
 
