40a84c9cf19bb7d2a1d9612e4bf7ccb5de005077
Markus Heiliger <markus.heiliger@dash-two.de>
2024-09-18 17:32:44 +0200

    exclude cleanmgr for win10

--- _scripts/core/Generalize-VM.ps1
+++ _scripts/core/Generalize-VM.ps1
@@ -58,11 +58,14 @@ Invoke-ScriptSection -Title 'Optimize Windows Partition' -ScriptBlock {
 	Write-Host ">>> Check Windows Component Store Health"
 	Invoke-CommandLine -Command 'dism' -Arguments '/Online /Cleanup-Image /CheckHealth' | Select-Object -ExpandProperty Output | Write-Host
 
-	Write-Host ">>> Prepare Disk Cleanup Utility"
-	Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches" | ForEach-Object { New-ItemProperty -Path "$($_.PSPath)" -Name StateFlags0000 -Value 2 -Type DWORD -Force | Out-Null }
+	if (Test-IsWindows11) {
 
-	Write-Host ">>> Run Disk Cleanup Utility"
-	Invoke-CommandLine -Command 'cleanmgr' -Arguments '/verylowdisk /sagerun:0' | Select-Object -ExpandProperty Output | Write-Host
+		Write-Host ">>> Prepare Disk Cleanup Utility"
+		Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches" | ForEach-Object { New-ItemProperty -Path "$($_.PSPath)" -Name StateFlags0000 -Value 2 -Type DWORD -Force | Out-Null }
+
+		Write-Host ">>> Run Disk Cleanup Utility"
+		Invoke-CommandLine -Command 'cleanmgr' -Arguments '/verylowdisk /sagerun:0' | Select-Object -ExpandProperty Output | Write-Host
+	}
 
 	Write-Host ">>> Run free space consolidation"
 	Invoke-CommandLine -Command 'defrag' -Arguments 'c: /FreespaceConsolidate /Verbose'
