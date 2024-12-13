075eac7fc4ec77941e40abe5cb4653cc0ef0e623
Markus Heiliger <markus.heiliger@dash-two.de>
2024-12-13 14:14:53 +0100

    Enhance Install-WinGet.ps1 by adding additional logging for package installation errors to improve troubleshooting

--- _scripts/pkgs/Install-WinGet.ps1
+++ _scripts/pkgs/Install-WinGet.ps1
@@ -58,6 +58,8 @@ function Install-Package() {
 				Write-Warning $exceptionMessage
 				Write-Host "----------------------------------------------------------------------------------------------------------"
 				Get-AppxLog -ActivityId $_ | Out-Host
+				Write-Host "----------------------------------------------------------------------------------------------------------"
+				Get-AppPackageLog -ActivityID $_ | Out-Host
 			}
 
 			throw
