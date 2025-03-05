35ef838389fe837ea0e50811a603f59fcbf6dc31
Markus Heiliger <markus.heiliger@microsoft.com>
2025-03-05 17:54:41 +0100

    Update HANA Express Docker run command to use the correct image reference #HANABOX#

--- _scripts/Configure-HanaExpress.ps1
+++ _scripts/Configure-HanaExpress.ps1
@@ -58,7 +58,7 @@ if (Test-IsPacker) {
         @{ "master_password" = "HXEHana1" } | ConvertTo-Json | Set-Content -Path $hanaSettings -Force
 
         Write-Host ">>> Starting HANA Express ..."
-        Invoke-CommandLine -Command $docker -Arguments "run -p 39013:39013 -p 39017:39017 -p 39041-39045:39041-39045 -p 1128-1129:1128-1129 -p 59013-59014:59013-59014 -d -v ${$hanaHome}:/hana/mounts --ulimit nofile=1048576:1048576 --sysctl kernel.shmmax=1073741824 --sysctl net.ipv4.ip_local_port_range='40000 60999' --sysctl kernel.shmmni=524288 --sysctl kernel.shmall=8388608 --name 'HANA-Express' store/saplabs/hanaexpress:2.00.045.00.20200121.1 --passwords-url file:///hana/mounts/settings.json --agree-to-sap-license" | Select-Object -ExpandProperty Output | Write-Host
+        Invoke-CommandLine -Command $docker -Arguments "run -p 39013:39013 -p 39017:39017 -p 39041-39045:39041-39045 -p 1128-1129:1128-1129 -p 59013-59014:59013-59014 -d -v ${$hanaHome}:/hana/mounts --ulimit nofile=1048576:1048576 --sysctl kernel.shmmax=1073741824 --sysctl net.ipv4.ip_local_port_range='40000 60999' --sysctl kernel.shmmni=524288 --sysctl kernel.shmall=8388608 --name 'HANA-Express' saplabs/hanaexpress --passwords-url file:///hana/mounts/settings.json --agree-to-sap-license" | Select-Object -ExpandProperty Output | Write-Host
     
     }
 }
