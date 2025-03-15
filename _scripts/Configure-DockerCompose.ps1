eabbfd8458c4373b4e84be7e348c81202ec605c5
Markus Heiliger <markus.heiliger@microsoft.com>
2025-03-15 13:08:55 +0100

    Refactor Docker Compose configuration and update script for improved clarity and functionality #HANABOX#

--- _scripts/Configure-DockerCompose.ps1
+++ _scripts/Configure-DockerCompose.ps1
@@ -8,7 +8,7 @@ Get-ChildItem -Path (Join-Path $env:DEVBOX_HOME 'Modules') -Directory | Select-O
 
 if (Test-IsPacker) {
 	Write-Host ">>> Register ActiveSetup"
-	Register-ActiveSetup  -Path $MyInvocation.MyCommand.Path -Name 'Configure-HanaExpress.ps1'
+	Register-ActiveSetup  -Path $MyInvocation.MyCommand.Path -Name 'Configure-DockerCompose.ps1'
 } else { 
     Write-Host ">>> Initializing transcript"
     Start-Transcript -Path ([system.io.path]::ChangeExtension($MyInvocation.MyCommand.Path, ".log")) -Append -Force -IncludeInvocationHeader; 
@@ -23,25 +23,24 @@ if (-not (Test-IsPacker)) {
 
     Invoke-ScriptSection -Title "Configure Docker Compose" -ScriptBlock {
 
-        if ( -and (Start-Docker)) {
+        if (Start-Docker) {
 
             # ensure docker artifacts directory exists
             $dockerArtifacts = (New-Item -Path (Join-Path $env:DEVBOX_HOME 'Artifacts\Docker') -ItemType Directory -Force).FullName
 
-            $dockerEnvironment = @{
-                "ROOT" = "/$($workingDirectory.Replace('\', '/').Replace(':', ''))"
-            }
-
             Get-ChildItem -Path $dockerArtifacts -Filter 'compose.yml' -Recurse -File | Select-Object -ExpandProperty FullName | ForEach-Object {
+                
+                $composeDirectory = Split-Path $_ -Parent
+                $composeEnvironment = @{ "ROOT" = "/$($composeDirectory.Replace('\', '/').Replace(':', ''))" }
 
-                $envFile = Join-Path (Split-Path $_ -Parent) '.env'
+                $envFile = Join-Path $composeDirectory '.env'
                 $envVars = "$(Get-Content -Path $envFile -Raw -ErrorAction SilentlyContinue)"
 
                 $envVars = (($envVars -split "`r?`n" | Where-Object { 
                     if ([string]::IsNullOrWhiteSpace($_)) { return $false }
                     $key = "$(($_ -split '=') | Select-Object -First 1 -ErrorAction SilentlyContinue)".Trim()
-                    return (-not $dockerEnvironment.ContainsKey($key))
-                }) + ($dockerEnvironment.Keys | ForEach-Object { "$_=$($dockerEnvironment[$_])" })) | Out-String
+                    return (-not $composeEnvironment.ContainsKey($key))
+                }) + ($composeEnvironment.Keys | ForEach-Object { "$_=$($composeEnvironment[$_])" })) | Out-String
                 
                 Write-Host ">>> Writing environment variables to .env file: $envFile"
                 $envVars | Set-Utf8Content -Path $envFile -ErrorAction SilentlyContinue
