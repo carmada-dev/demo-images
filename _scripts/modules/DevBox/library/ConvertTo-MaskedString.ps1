56b04502b7da40b510a6a592171ad56e38f82ae8
Markus Heiliger <markus.heiliger@microsoft.com>
2025-03-18 10:46:59 +0100

    Enhance ConvertTo-MaskedString function: add check for non-empty mask array before masking values #ALL#

--- _scripts/modules/DevBox/library/ConvertTo-MaskedString.ps1
+++ _scripts/modules/DevBox/library/ConvertTo-MaskedString.ps1
@@ -7,7 +7,8 @@ function ConvertTo-MaskedString {
         [string[]] $Mask = @()
     )
 
-    if ($Value -and $Mask) { 
+    if ($Value -and $Mask -and $Mask.Count -gt 0) {
+        # replace all mask values with asterisks - we start with the longest mask first to ensure that we do not mask a part of a longer mask 
         $Mask | Sort-Object -Descending { $_.length } | ForEach-Object { $Value = $Value -replace $_, ('*' * $_.Length) } 
     } 
 
