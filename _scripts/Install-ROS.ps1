4469e93fabf7132add35110a54bb1b2d204af5e3
Markus Heiliger <markus.heiliger@dash-two.de>
2025-10-10 01:20:30 +0200

    Fix syntax in ROS source list echo command for proper variable expansion #ROSBox#

--- _scripts/Install-ROS.ps1
+++ _scripts/Install-ROS.ps1
@@ -29,7 +29,7 @@ set -e
 # Setup sources
 sudo apt update && sudo apt install -y curl gnupg lsb-release
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=\$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu \$(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+echo "deb [arch=`$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu `$(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 # Install ROS
 sudo apt update 
