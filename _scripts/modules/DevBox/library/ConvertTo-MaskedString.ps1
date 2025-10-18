ad5f54eee4e7186a2ab2ab5a6330b7fbb070ad8b
Markus Heiliger <markus.heiliger@dash-two.de>
2025-10-18 15:29:07 +0200

    Fix masking logic in ConvertTo-MaskedString function to exclude empty mask values before replacement #ROSBox#

--- _scripts/modules/DevBox/library/ConvertTo-MaskedString.ps1
+++ _scripts/modules/DevBox/library/ConvertTo-MaskedString.ps1
@@ -9,7 +9,7 @@ function ConvertTo-MaskedString {
 
     if ($Value) {
         # replace all mask values with asterisks - we start with the longest mask first to ensure that we do not mask a part of a longer mask 
-        $Mask | Sort-Object -Descending { $_.length } | ForEach-Object { $Value = "$Value".Replace($_, ('*' * $_.Length)) } 
+        $Mask | Where-Object { "$_" -ne '' } | Sort-Object -Descending { "$_".length } | ForEach-Object { $Value = "$Value".Replace($_, ('*' * $_.Length)) } 
     }
 
     return $Value
